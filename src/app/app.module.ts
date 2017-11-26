b385731df3088e3deb7150c03f45af9fbf320974
Christophe Ayel <christophe.ayel@gmail.com>
2017-11-26 22:23:50 +0100

    Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
+import { environment } from '../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+//import { AngularFireDatabaseModule } from 'angularfire2/database';
+//import { AngularFireAuthModule} from 'angularfire2/auth';
+
+
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { HomeComponent } from './home/home.component';
+import { MyBooksComponent } from './my-books/my-books.component';
+import { MyMoviesComponent } from './my-movies/my-movies.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BsNavbarComponent
+    BsNavbarComponent,
+    HomeComponent,
+    MyBooksComponent,
+    MyMoviesComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+//    AngularFireDatabaseModule,
+//    AngularFireAuthModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path : 'my-books', component: MyBooksComponent},
+      { path : 'my-movies', component: MyMoviesComponent },
+      { path : 'login', component: LoginComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
