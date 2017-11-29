efe74be0943140054baf0cb6ded9e5bcbdd8fbcd
Christophe Ayel <christophe.ayel@gmail.com>
2017-11-29 20:58:54 +0100

    Searching books

---
+++ src/app/models/book.ts
@@ -0,0 +1,7 @@
+export interface Book {
+    title: string;
+    writer: string;
+    date: Date;
+    rating: number;
+    imageUrl: string;
+}
--- src/app/my-books/my-books.component.ts
+++ src/app/my-books/my-books.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { BookService } from '../book.service';
 import { Subscription } from 'rxjs/Subscription';
 import { AuthService } from '../auth.service';
+import { Book } from '../models/book';
 
 @Component({
   selector: 'app-my-books',
@@ -11,17 +12,26 @@ import { AuthService } from '../auth.service';
 export class MyBooksComponent implements OnInit, OnDestroy {
   userSubscription: Subscription;
   userId: string;
-  books$;
+  books: Book[];
+  filteredBooks: Book[];
+  bookSubscription: Subscription
 
   constructor(private authService: AuthService, private bookService: BookService) {
     this.userSubscription = this.authService.user$.subscribe(user => this.userId = user.uid);
    }
 
+  filter(query: string) {
+    this.filteredBooks = (query) ?
+      this.books.filter(b => b.title.toLowerCase().includes(query.toLowerCase())) : 
+      this.books;
+  }
+
   ngOnInit() {
-    this.books$ = this.bookService.getAll(this.userId); 
+    this.bookSubscription = this.bookService.getAll(this.userId).subscribe(books => this.filteredBooks = this.books = books); 
   }
 
   ngOnDestroy() {
     this.userSubscription.unsubscribe();
+    this.bookSubscription.unsubscribe();
   }
 }
