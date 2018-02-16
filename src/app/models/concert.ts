f23db35da0841a794a7522590672f8c646f760aa
Christophe Ayel <CAYEL@cegid.fr>
2018-02-16 12:58:10 +0100

    Add concert comment

--- src/app/models/concert.ts
+++ src/app/models/concert.ts
@@ -1,4 +1,4 @@
 export class Concert {
-    constructor(public mainArtist : string, public otherArtist : string, public date : number, public venue : string, public rating : number) {
+    constructor(public mainArtist : string, public otherArtist : string, public date : number, public venue : string, public rating : number, public comment: string) {
     }
 }
