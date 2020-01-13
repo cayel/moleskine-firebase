7cbe9e1f933e14933e3bfb52097e98e8ed5a0d0e
Christophe Ayel <christophe.ayel@gmail.com>
2020-01-13 21:26:23 +0100

    angular5

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit,OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { Movie } from '../models/movie';
-import { DataTableResource } from 'angular-4-data-table';
+import { DataTableResource } from 'angular5-data-table';
 import { AuthService } from '../auth.service';
 import { MovieService } from '../movie.service';
 
